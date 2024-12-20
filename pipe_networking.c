974f42e77fe60ef7c0e63bc333389b25bd8bbb6a
Raechel Grace Calungsod <raegrasc13@gmail.com>
2024-12-20 15:30:31 -0500

    must continue... variable names?

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,4 +1,11 @@
 #include "pipe_networking.h"
+
+//prints errno
+int err(){
+  printf("Error %d: %s\n", errno, strerror(errno));
+  exit(1);
+}
+
 // UPSTREAM = to the server / from the client
 // DOWNSTREAM = to the client / from the server
 /*=========================
@@ -12,12 +19,12 @@
 int server_setup() {
   int from_client = 0;
   // make WKP
-  int well_known = mkfifo(WKP, 0644); // er,,,.
-  if (well_known==-1) perror("cant make WKP");
+  from_client = mkfifo(WKP, 0644); // er,,,.
+  if (from_client==-1) err();
 
   // open wkp.[blocks]
-  int open_wkp = open(WKP,  O_WRONLY | O_CREAT | O_TRUNC, 0600); // i dont know about perms
-  if (open_wkp==-1) perror("can't open wkp");
+  int open_wkp = open(from_client,  O_RDONLY, 0644); // i dont know about perms
+  if (open_wkp==-1) err();
 
   // step 3 in client
 
@@ -36,20 +43,22 @@ int server_setup() {
 
   returns the file descriptor for the upstream pipe (see server setup).
   =========================*/
-int server_handshake(int *to_client) {
+int server_handshake(int *to_client) { // im confused... do i use to_server or to_client for wkp and pp? :P to_client = pp...?
   int from_client;
 
   // calls server_setup();
   server_setup();
 
   // server reading SYN (pid)
-
+  int my_syn = getpid();
 
   // server opening private pipe [unblock client]
-  int open_pp = open("pp",  O_WRONLY | O_CREAT | O_TRUNC, 0600); // i dont know about flags here
-  if (open_pp==-1) perror("can't open pp");
+  from_client = open("pp",  O_WRONLY | O_CREAT, 0600); // i dont know about flags here
+  if (from_client==-1) err();
 
   // server send syn_ack
+  int w = write(from_client, my_syn, 64);
+  if (w==-1) err();
 
   // step 8 in client
 
@@ -69,25 +78,27 @@ int server_handshake(int *to_client) {
 
   returns the file descriptor for the downstream pipe.
   =========================*/
-int client_handshake(int *to_server) {
+int client_handshake(int *to_server) { // im confused... do i use to_server or to_client for wkp and pp? :P to_server = WKP...?
   int from_server;
 
   // at step 3, make private pipe
   int pp = mkfifo("pp", 0644);
-  if (pp == -1) perror("private pipe cannot be made");
+  if (pp == -1) err();
 
   int pp_pid;
   if (pp == 0) pp_pid = getpid();
 
   // open wkp[unblock server]
   int open_wkp = open(WKP,  O_WRONLY | O_CREAT | O_TRUNC, 0600); // i dont know about flags here
-  if (open_wkp==-1) perror("can't open wkp");
+  if (open_wkp==-1) err();
 
   // client writing pp to wkp (match w step 5)
-  write(open_wkp, pp_pid, 64);
+  int w = write(open_wkp, pp_pid, 64);
+  if (w == -1) err();
 
   // step 8, client deleting pp
-  remove(pp);
+  int r = remove(pp);
+  if (r==-1) err();
 
   // client reading syn_ack (matches w step 7)
 
